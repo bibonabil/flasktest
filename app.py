aee144d5466df95969844e87b7f7c1c82d3e1176
bibonabil <bobsy.test@gmail.com>
2026-06-17 19:54:26 +0300

    flask copy

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello my name is abdelrahman this is the project 1 test 3 "
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
